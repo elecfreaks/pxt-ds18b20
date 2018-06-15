4b0a3732c4eb122186a33194ae9f9822c88c1bfb
mengbishu <841616394@qq.com>
2018-06-15 16:33:56 +0800

    1

--- ds1820.ts
+++ ds1820.ts
@@ -33,10 +33,9 @@ namespace DS1820 {
     //% weight=10 blockId="DS1820_Temperature" 
     //% shim=DS1820::Temp
     //% block="Temp(C)"
-    export function Temp() {
-        var temp = Temperature()
-        var data1 = (temp/100).toString
-        return data1.toString
-        }
+    export function Temp() : string{
+        var temp = Temperature();
+        var data1 = (temp/100).toString();
+        return data1
     }
 }
