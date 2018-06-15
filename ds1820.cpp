00228583a8ab2cb6fe4b7082db713e4a7846af19
mengbishu <841616394@qq.com>
2018-06-15 14:18:33 +0800

    1

--- ds1820.cpp
+++ ds1820.cpp
@@ -72,9 +72,9 @@ class microbitp : public MicroBitComponent
     
 //    MicroBitPin pin1 = uBit.io.P1;
 //    MicroBit uBit;
-    MicroBitPin pin1 = uBit.io.P2;
+//    MicroBitPin pin1 = uBit.io.P2;
 //    microbitp  pin0(7, 3, 15);
-//    microbitp  pin0(8, MICROBIT_PIN_P0, PIN_CAPABILITY_ALL);
+    microbitp  pin1(8, MICROBIT_PIN_P0, PIN_CAPABILITY_ALL);
 //    microbitp  pin2(9, 1, 15);
 
     uint8_t init() {
