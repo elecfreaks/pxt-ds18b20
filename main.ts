36cc1ab7a21e16f6dac1a2a7da739e44f589578e
ELECFREAKS <services@elecfreaks.com>
2019-04-30 16:05:08 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -44,7 +44,7 @@ namespace DS18B20 {
     export function TemperatureNumber(p: pin): number {
         // Fake function for simulator
         let temp = Temperature(p)
-        temp = temp /10
+        temp = temp / 10
         return temp
     }
 }
