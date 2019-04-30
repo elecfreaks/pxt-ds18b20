6f413f99032247750b0da7d69c5dc3b3446c81f8
Lionkk <32665689+lionyhw@users.noreply.github.com>
2019-04-30 10:54:28 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -6,6 +6,31 @@ namespace DS18B20 {
      *temperature in hundreths of a degree centigrade. 
      * block="Temperature(C)"
      */
+
+     export enum pin {
+       //% block=pin0
+       pin0 = 0,
+       //% block=pin1
+       pin1 = 1,
+       //% block=pin2
+       pin2 = 2,
+       //% block=pin5
+       pin5 = 5,
+       //% block=pin8
+       pin8 = 8,
+       //% block=pin11
+       pin11 = 11,
+       //% block=pin12
+       pin12 = 12,
+       //% block=pin13
+       pin13 = 13,
+       //% block=pin14
+       pin14 = 14,
+       //% block=pin15
+       pin15 = 15,
+       //% block=pin16
+       pin16 = 16
+     }
      
     //% shim=DS18B20::Temperature
     export function Temperature(p: number): number {
